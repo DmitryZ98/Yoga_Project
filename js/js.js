a1ae8473cb328e3990daeb55893f06becfed88c5
Dmitry <DmitryZaxlevnyi98kursk@yandex.ru>
2019-08-17 20:43:30 +0300

    create modal window

--- js/js.js
+++ js/js.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     //Создание таймера
-    let deadLine = '2019-08-18';
+    let deadLine = '2019-08-17';
 
     function getTimeRemaining(endtime){
         let t = Date.parse(endtime) - Date.parse(new Date());
@@ -93,5 +93,33 @@ window.addEventListener('DOMContentLoaded', function() {
             }
     }
     setClock('timer',deadLine);
+
+ //Create Modal window
+    let more = document.querySelector('.more');
+    let overlay = document.querySelector('.overlay');
+    let close = document.querySelector('.popup-close');
+    let btnDescr = document.querySelectorAll('.description-btn');
+    
+    more.addEventListener('click', function(){
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        this.classList.remove('more-splash');
+        document.body.style.overflow = '';
+});
+
+//Открываем подробную информацию на каждом табе
+    [].forEach.call(btnDescr,function(event){
+        event.addEventListener('click',function(){
+            overlay.style.display = "block";
+            this.classList.add('more-splash');
+            document.body.style.overflow = "hidden";
+        });
+    });
+
 });
 
